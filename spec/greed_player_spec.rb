e6eac58d717745c55b2e75f15384e78232cdfa4d
“Tinisi” <jgrosz@tinisi.com>
2016-05-09 08:30:47 -0500

    got rspec class spy working to some degree

--- spec/greed_player_spec.rb
+++ spec/greed_player_spec.rb
@@ -3,11 +3,9 @@ require_relative '../koans/greed_player'
 
 describe GreedPlayer do
   it "calls through to DiceSet when roll is called" do
-    dice = instance_spy("DiceSet")
+    expect_any_instance_of(DiceSet).to receive(:roll).and_call_original
     player = GreedPlayer.new("", :high)
     expect(player.risk_tolerance).to be :high
-    # TODO: not quite working yet!
-    # foo = player.roll(2)
-    # expect(dice).to have_received(:roll)
+    foo = player.roll(2)
   end
 end
